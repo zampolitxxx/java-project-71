c67ac3a2587faf4a86b4778a8b1c3f5dce194a0c
zampolitxxx <zampolitxxx@gmail.com>
2023-09-04 15:57:55 +0900

    add picocli
    add picocli to the build.gradle
    add picocli to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,29 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        version = "gendiff 0.1", description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable{
+
+    @Override
+    public Integer call() throws Exception {
+        try {
+            System.out.println("hello");
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
 }
