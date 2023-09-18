3d47be799d27c7ec2a20d1eb6d1145e7ee6aa309
zampolitxxx <zampolitxxx@gmail.com>
2023-09-18 13:59:16 +0900

    add simple test
    void test for Differ.generate

---
+++ app/src/test/java/DifferTest.java
@@ -0,0 +1,16 @@
+/*
+ * Created by zampolit 2023.09.18 at 10.47
+ * version info - v0.1
+ * Classname - DifferTest.java
+ * This class performs unit testing of the generate() method
+ */
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class DifferTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
