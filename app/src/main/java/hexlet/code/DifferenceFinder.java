be8cf7b785c88264314315c2caae693af2695500
zampolitxxx <zampolitxxx@gmail.com>
2023-10-06 11:59:02 +0900

    replace nested if-else-if to simple else-if in DifferenceFinder.java

--- app/src/main/java/hexlet/code/DifferenceFinder.java
+++ app/src/main/java/hexlet/code/DifferenceFinder.java
@@ -19,18 +19,12 @@ public class DifferenceFinder {
         for (String key: keys) {
             if (!map1.containsKey(key)) {
                 result.put(key, new Data("added", null, map2.get(key)));
-            } else if (map1.containsKey(key)) {
-                Object val1 = map1.get(key);
-                if (!map2.containsKey(key)) {
-                    result.put(key, new Data("deleted", map1.get(key), null));
-                } else {
-                    Object val2 = map2.get(key);
-                    if (Objects.equals(val1, val2)) {
-                        result.put(key, new Data("unchanged", map1.get(key), map2.get(key)));
-                    } else {
-                        result.put(key, new Data("changed", map1.get(key), map2.get(key)));
-                    }
-                }
+            } else if (!map2.containsKey(key)) {
+                result.put(key, new Data("deleted", map1.get(key), null));
+            } else if (Objects.equals(map1.get(key), map2.get(key))) {
+                result.put(key, new Data("unchanged", map1.get(key), map2.get(key)));
+            } else if (!Objects.equals(map1.get(key), map2.get(key))) {
+                result.put(key, new Data("changed", map1.get(key), map2.get(key)));
             }
         }
         return result;
