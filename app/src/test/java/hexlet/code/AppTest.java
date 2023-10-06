ce651f88f79216b85e675a8a4639ae35d75062fb
zampolitxxx <zampolitxxx@gmail.com>
2023-10-06 11:02:05 +0900

    make AppTest class to final

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class AppTest {
+public final class AppTest {
     private static String expectedStylish;
     private static String expectedPlain;
     private static String expectedJson;
