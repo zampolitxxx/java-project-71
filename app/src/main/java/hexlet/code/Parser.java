446aff813f491d87094ae176672352919a0f01b7
zampolitxxx <zampolitxxx@gmail.com>
2023-10-11 15:38:51 +0900

    refactor case operator
    it was "case "json" -> return new mapper..."
    become "return switch (extension) {
       case "json" -> new mapper...

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -6,14 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Parser {
     public static Map<String, Object> parse(String str, String extension) throws Exception {
-        switch (extension) {
-            case "json" -> {
-                return new ObjectMapper().readValue(str, Map.class);
-            }
-            case "yml", "yaml" -> {
-                return new YAMLMapper().readValue(str, Map.class);
-            }
+        return switch (extension) {
+            case "json" -> new ObjectMapper().readValue(str, Map.class);
+            case "yml", "yaml" -> new YAMLMapper().readValue(str, Map.class);
             default -> throw new RuntimeException("Unsupported format: " + extension);
-        }
+        };
     }
 }
