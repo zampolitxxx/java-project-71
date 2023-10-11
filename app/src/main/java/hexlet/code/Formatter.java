13d3e421882e4c8f5f720990f7994d2923f851f4
zampolitxxx <zampolitxxx@gmail.com>
2023-10-11 15:54:16 +0900

    reformat case operator
    it was:
    switch(format)
       case "stylish" -> return
    
    become:
    return switch(format)
       case "stylish" ->

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -7,22 +7,12 @@ import java.util.Map;
 
 public class Formatter {
     public static String create(Map<String, Data> data, String format) throws Exception {
-        String result;
-        switch (format) {
-            case "stylish" -> {
-                result = Stylish.format(data);
-            }
-            case "plain" -> {
-                result = Plain.format(data);
-            }
-            case "json" -> {
-                result = JSON.format(data);
-            }
-            default -> {
-                throw new RuntimeException("Formatter.java: format "  + "'" + format + "'" + " is wrong!");
-            }
-        }
-        return result;
+        return switch (format) {
+            case "stylish" -> Stylish.format(data);
+            case "plain" -> Plain.format(data);
+            case "json" -> JSON.format(data);
+            default -> throw new RuntimeException("Formatter.java: format "  + "'" + format + "'" + " is wrong!");
+        };
     }
 
 
